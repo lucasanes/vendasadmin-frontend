65d3cbbbeaa6611a89b5ef08406f964ea699026d
SkyLuke11 <sky.lucas003@gmail.com>
2024-04-12 21:50:02 -0300

    Update API base URL to production environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://novosigeve-production.up.railway.app",
+  // baseURL: "http://localhost:8081",
 });
